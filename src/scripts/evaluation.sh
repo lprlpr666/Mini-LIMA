89385700ad92c6e9dfb2308a769f2815856adff5
lprlpr666 <1578231761@qq.com>
2024-04-22 09:54:41 +0800

    only hastin need so

--- src/scripts/evaluation.sh
+++ src/scripts/evaluation.sh
@@ -1,10 +1,10 @@
 # use HF mirror endpoint so that you can access the datasets properly on DSW notebook.
 export HF_ENDPOINT=https://hf-mirror.com
 
-# copy your model config into alpaca folder
+# copy your model config into alpaca folder 
 cp -r mini_lima/ alpaca_eval/src/alpaca_eval/models_configs/
 # cp -r mini_lima/ /home/zsq259/.conda/envs/alpaca_env/lib/python3.10/site-packages/alpaca_eval/models_configs/
-cp -r mini_lima/ /home/zsq259/.local/lib/python3.11/site-packages/alpaca_eval/models_configs/
+# cp -r mini_lima/ /home/zsq259/.local/lib/python3.11/site-packages/alpaca_eval/models_configs/
 
 alpaca_eval evaluate_from_model \
   --model_configs 'mini_lima' \
